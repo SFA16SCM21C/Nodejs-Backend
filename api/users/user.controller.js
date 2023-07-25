41941b22e5e6d2ae35c6197e12bfca5edc7dec95
Saloni Chauhan <schauhan1@hawk.iit.edu>
2023-07-24 19:35:18 -0500

    first commit

---
+++ api/users/user.controller.js
@@ -0,0 +1,24 @@
+const { create } = require("./user.service");
+
+const { genSaltSync, hashSync } = require("bcrypt");
+
+module.exports = {
+    createUser: (req, res) => {
+        const body = req.body;
+        const salt = genSaltSync();
+        body.password = hashSync(body.password, salt);
+        create(body, (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status().json({
+                    sucess: 0,
+                    message: "Db connection error"
+                });
+            }
+            return res.status(200).json({
+                sucess: 1,
+                data: results
+            });
+        })
+    }
+};
