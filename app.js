71f08a245caaccabd6f4ea4a1444efab4247f14d
Saloni Chauhan <schauhan1@hawk.iit.edu>
2023-07-25 20:13:09 -0500

    comments added

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const userRouter = require("./api/users/user.router")
 //    });
 //});
 
+app.use(express.json());
+
+app.use("/api/users", userRouter);
+
 app.listen(process.env.APP_PORT, () => {
     console.log("Server up and running on PORT : ", process.env.APP_PORT);
 });
