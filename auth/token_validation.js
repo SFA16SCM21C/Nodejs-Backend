e6681ea1b73559e3ea4dd9c37c4244a9d640a967
Saloni Chauhan <schauhan1@hawk.iit.edu>
2023-07-27 22:27:37 -0500

    tokens added and token checked

--- auth/token_validation.js
+++ auth/token_validation.js
@@ -2,10 +2,19 @@ const { verify } = require("jsonwebtoken");
 
 module.exports = {
     checkToken: (req, res, next) => {
-        const token = req.get("authorized");
+        let token = req.get("authorized");
         if (token) {
             token = token.slice(7);
-            verify(token, "");
+            verify(token, "qwe1234", (err, decoded) => {
+                if (err) {
+                    res.json({
+                        success: 0,
+                        message: "Invalid token"
+                    });
+                } else {
+                    next();
+                }
+            });
         } else {
             res.json({
                 success: 0,
