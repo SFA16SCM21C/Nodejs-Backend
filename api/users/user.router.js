d1a0549d3061f41f2590d8a63c339d579564e954
Saloni Chauhan <schauhan1@hawk.iit.edu>
2023-07-27 21:41:42 -0500

    routes added too

--- api/users/user.router.js
+++ api/users/user.router.js
@@ -1,6 +1,10 @@
-const { createUser } = require("./user.controller");
+const { createUser, getUserById, getUsers, updateUsers, deleteUser } = require("./user.controller");
 const router = require("express").Router();
 
 router.post("/", createUser);
+router.get("/", getUsers);
+router.get("/:id", getUserById);
+router.patch("/", updateUsers);
+router.delete("/", deleteUser);
 
 module.exports = router;
